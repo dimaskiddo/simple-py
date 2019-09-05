c0adfe2045716c885d82f3ff98aeb113c7fbf8c9
Dimas Restu H <dimas.restu@student.upi.edu>
2019-09-05 13:12:00 +0700

    update app.py to accept redis environment configuration

--- app.py
+++ app.py
@@ -5,14 +5,21 @@ from flask import Flask
 from redis import Redis, RedisError
 
 
-# Declare Library
+# Redis Connection Variable
+redis_host = os.getenv("REDIS_HOST", "127.0.0.1")
+redis_port = os.getenv("REDIS_PORT", "6379")
+redis_pass = os.getenv("REDIS_PASSWORD", None)
+
+
+# Library Variable
 app = Flask(__name__)
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+redis = Redis(host=redis_host, port=redis_port, password=redis_pass,
+              db=0, socket_connect_timeout=2, socket_timeout=2)
 
 
 # Main Application
 @app.route("/")
-def home():
+def index():
     try:
         visits = redis.incr("counter")
     except RedisError:
@@ -24,5 +31,7 @@ def home():
     
     return html.format(name=os.getenv("NAME", "World"), hostname=socket.gethostname(), visits=visits)
 
+
+# Run Application
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
